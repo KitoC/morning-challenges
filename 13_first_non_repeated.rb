4e800f5e9fa82042d0bfb96b1273d6f7ed9b2c76
Kito Clark <kitoclark1989@gmail.com>
2018-03-27 09:53:24 +1000

    Solved (but with help)

--- 13_first_non_repeated.rb
+++ 13_first_non_repeated.rb
@@ -11,7 +11,7 @@
 # indentation and be consistent in your decisions
 # (e.g. stick to single or double quotes)
 #
-# Difficulty: ~6/10 (Varies depending on solution. 
+# Difficulty: ~6/10 (Varies depending on solution.
 # The more optimised the solution, the more difficult
 # the algorithm.)
 #
@@ -31,6 +31,13 @@
 #
 
 def firstNonRepeat (string)
-  # Your code here
-  # Whiteboard first!
+
+  char_arr = string.split("")
+
+  #first attempt
+  char_arr.each do |c|
+    return c if char_arr.count(c) == 1
+  end
+  return false
+
 end
