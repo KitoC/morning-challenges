7ce61e8e6ad7d30506a8835c5105113c1f794f37
Kito Clark <kitoclark1989@gmail.com>
2018-04-17 10:12:44 +1000

    income tax solved

--- 18_income_tax.rb
+++ 18_income_tax.rb
@@ -15,5 +15,48 @@
 #
 
 def calculate_income_tax (income)
+  tax_return = {}
+  tax_return[:income] = income
+
+  if income >= 180001
+    tax_deductable = income - 180000
+    tax_over = tax_deductable * 0.45
+    tax = tax_over + 54232
+    tax_return[:tax] = tax
+    income_after_tax = income - tax
+    tax_return[:income_after_tax] = income_after_tax
+
+  elsif income <= 180000 && income >= 87001
+    tax_deductable = income - 87000
+    tax_over = tax_deductable * 0.37
+    tax = tax_over + 19822
+    tax_return[:tax] = tax
+    income_after_tax = income - tax
+    tax_return[:income_after_tax] = income_after_tax
+
+  elsif income <= 87000 && income >= 37001
+    tax_deductable = income - 37000
+    tax_over = tax_deductable * 0.325
+    tax = tax_over + 3572
+    tax_return[:tax] = tax
+    income_after_tax = income - tax
+    tax_return[:income_after_tax] = income_after_tax
+
+  elsif income <= 37000 && income >= 18201
+    tax_deductable = income - 18200
+    tax_over = tax_deductable * 0.19
+    tax = tax_over
+    tax_return[:tax] = tax
+    income_after_tax = income - tax
+    tax_return[:income_after_tax] = income_after_tax
+    
+  elsif income <= 18200
+    tax_return[:tax] = 0
+    tax_return[:income_after_tax] = income
+  end
+  return tax_return
+
   # Your code here!
 end
+
+calculate_income_tax (17000)
